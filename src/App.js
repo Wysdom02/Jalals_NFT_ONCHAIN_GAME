66d99f3bf6eda4966493b02aa2787fc35671e2be
hardXcoder <ujjwal451@gmail.com>
2022-09-01 23:08:21 +0530

    Resolving conflicts

--- src/App.js
+++ src/App.js
@@ -3,123 +3,117 @@ import "./App.css";
 import Mint from "./components/Mint.js";
 import Attack from "./components/Attack.js";
 import Home from "./components/Home.js";
-import Web3 from 'web3';
-import { Component } from 'react';
+import Web3 from "web3";
+import { Component } from "react";
 import NavbarB from "./components/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/Layout";
 import ConnectButton from "./components/connectButton";
 import Stake from "./components/Stake";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer.js";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-
-
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends Component {
-
-
-  
   constructor(props) {
     super(props);
-    this.state  = {
-      account:'',
+    this.state = {
+      account: "",
       connected: false,
-      nftmeta1:"https://i.insider.com/61cc84b94710b10019c77960?width=700",
-      nftdesc1:"All about your awesome NFT",
-      nftname1:" ",
-      nftlink1:"#",
-      nftmeta2:"https://s.yimg.com/os/creatr-uploaded-images/2022-04/61254c10-c4e2-11ec-b6eb-8947bb0d6d4e",
-      nftdesc2:"All about your awesome NFT#2",
-      nftname2:" ",
-      nftlink2:"#",
-    }
-    this.loadBlockchain = this.loadBlockchain.bind(this)
-    this.getNFT = this.getNFT.bind(this)
+      nftmeta1: "https://i.insider.com/61cc84b94710b10019c77960?width=700",
+      nftdesc1: "All about your awesome NFT",
+      nftname1: " ",
+      nftlink1: "#",
+      nftmeta2:
+        "https://s.yimg.com/os/creatr-uploaded-images/2022-04/61254c10-c4e2-11ec-b6eb-8947bb0d6d4e",
+      nftdesc2: "All about your awesome NFT#2",
+      nftname2: " ",
+      nftlink2: "#",
+    };
+    this.loadBlockchain = this.loadBlockchain.bind(this);
+    this.getNFT = this.getNFT.bind(this);
   }
 
-  async getNFT(val,n) {
-    const nft = await fetch("https://api.opensea.io/api/v1/asset/" + val)
-    if(nft.status!==404 && nft.status!==429 && nft.status!==500)
-    {const data = await nft.json();
-      console.log(data)
-      if(n==1){ 
-      this.setState( { 
-        nftmeta1 : data.image_url , 
-        
-        nftname1: data.name,
-      } )
-      if(data.permalink!=null && data.description!=null)
-      { this.setState( { nftlink1: data.permalink,
-        nftdesc1 : data.description.slice(0,130)+"..",
-      } ) }
+  async getNFT(val, n) {
+    const nft = await fetch("https://api.opensea.io/api/v1/asset/" + val);
+    if (nft.status !== 404 && nft.status !== 429 && nft.status !== 500) {
+      const data = await nft.json();
+      console.log(data);
+      if (n == 1) {
+        this.setState({
+          nftmeta1: data.image_url,
+
+          nftname1: data.name,
+        });
+        if (data.permalink != null && data.description != null) {
+          this.setState({
+            nftlink1: data.permalink,
+            nftdesc1: data.description.slice(0, 130) + "..",
+          });
+        }
+      } else {
+        this.setState({
+          nftmeta2: data.image_url,
+          nftname2: data.name,
+        });
+        if (data.permalink != null && data.description != null) {
+          this.setState({
+            nftlink2: data.permalink,
+            nftdesc2: data.description.slice(0, 130) + "..",
+          });
+        }
       }
-      else {this.setState( { 
-        nftmeta2 : data.image_url , 
-        nftname2: data.name,
-      } )
-      if(data.permalink!=null && data.description!=null){ this.setState( { 
-        nftlink2: data.permalink,
-        nftdesc2 : data.description.slice(0,130)+".." } ) }
+    } else {
+      console.log("no nft");
     }
-    }
-    else 
-    {console.log("no nft")}
   }
 
+  async loadBlockchain() {
+    const web3 = new Web3(Web3.givenProvider);
+    await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    this.setState({ connected: true });
+    console.log("hello func");
+  }
 
- async loadBlockchain()
-{ 
-  const web3 = new Web3(Web3.givenProvider)
-  await window.ethereum.enable();
-  const accounts = await web3.eth.getAccounts()
-  this.setState( { account : accounts[0] } )
-  this.setState( {connected : true})
-  console.log("hello func")
+  render() {
+    return (
+      <div className="App">
+        {!this.state.connected ? (
+          <ConnectButton loadBlockchain={this.loadBlockchain} />
+        ) : (
+          console.log("connected")
+        )}
+        <NavbarB />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              path="attack"
+              element={
+                <Attack
+                  getNFT={this.getNFT}
+                  nftmeta1={this.state.nftmeta1}
+                  nftmeta2={this.state.nftmeta2}
+                  nftdesc1={this.state.nftdesc1}
+                  nftdesc2={this.state.nftdesc2}
+                  nftname1={this.state.nftname1}
+                  nftname2={this.state.nftname2}
+                  nftlink1={this.state.nftlink1}
+                  nftlink2={this.state.nftlink2}
+                />
+              }
+            />
+            <Route path="mint" element={<Mint />} />
+            <Route path="/stake" element={<Stake />} />
+          </Routes>
+        </Router>
 
-}
-
-  
-  render(){
-    
-  return (
-    <div className="App">
-      { !this.state.connected
-        ?<ConnectButton
-         loadBlockchain={this.loadBlockchain} />
-         :console.log("connected")
-      }<NavbarB/>
-       <Router>
-<Routes>
-<Route exact path="/" element={<Home/>}/>
-  <Route  path="attack" element={<Attack 
-  getNFT = {this.getNFT}
-  nftmeta1= {this.state.nftmeta1}
-  nftmeta2={this.state.nftmeta2}
-  nftdesc1 ={this.state.nftdesc1}
-  nftdesc2 ={this.state.nftdesc2}
-  nftname1={this.state.nftname1}
-  nftname2={this.state.nftname2}
-  nftlink1={this.state.nftlink1}
-  nftlink2={this.state.nftlink2}
-
-/>}/>
-  <Route  path="mint" element={<Mint/>}/>   
-  <Route path="/stake" element={<Stake/>}/>     
-</Routes>
-</Router>
-      
-
-      <Navbar></Navbar>
-      <Home />
-      <Footer></Footer>
-    </div>
-  );
-}
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,96 +1,59 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./cssfiles/navcss.css";
 
-
-
 function Collapsible() {
-
   return (
-    
     <div>
-     <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet"></link>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='trapeze'>
-      <Container >
-     
-        <Navbar.Brand href="/">Jalals</Navbar.Brand>
-        
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
-          <Nav>
-            <Nav.Link href="/mint">Mint</Nav.Link>
-            {/* <img
+      <link
+        href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap"
+        rel="stylesheet"
+      ></link>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        className="trapeze"
+      >
+        <Container>
+          <Navbar.Brand href="/">Jalals</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <Nav.Link href="/mint">Mint</Nav.Link>
+              {/* <img
               alt=""
               src="https://jalalstest.my.canva.site/videos/aced88fff16f7b9f612c332a878271e1.gif"
               width="30"
               height="30"
               className="d-inline-block align-top"
             />{' '} */}
-            
-            <Nav.Link eventKey={2} href="/attack">
-              Attack
-            </Nav.Link>
-            {/* <img
+
+              <Nav.Link eventKey={2} href="/attack">
+                Attack
+              </Nav.Link>
+              {/* <img
               alt=""
               src="https://jalalstest.my.canva.site/videos/37fff48731c5d92b578bb926cee42ee6.gif"
               width="30"
               height="30"
               className="d-inline-block align-top"
             />{' '} */}
-            <Nav.Link eventKey={2} href="/stake">
-              Stake
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      
-    </Navbar>
+              <Nav.Link eventKey={2} href="/stake">
+                Stake
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
 
 export default Collapsible;
-
-import React, { Component, Fragment } from "react";
-
-class Navbar extends Component {
-  connectToWallet = async () => {
-    try {
-      await window.ethereum.enable().then((account) => {
-        console.log("Connect button clicked !");
-        this.setState({ account: account[0] });
-        console.log(account[0]);
-      });
-      // this.loadWeb3();
-    } catch (error) {}
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: "0x0",
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Navbar</h3>
-        <button onClick={this.connectToWallet}>Connect Wallet </button>
-
-        {this.state.account != "0x0" ? (
-          <h4>{this.state.account}</h4>
-        ) : (
-          <div></div>
-        )}
-      </div>
-    );
-  }
-}
-
-export default Navbar;
